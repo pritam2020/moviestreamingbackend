31868831bb4b01f7547c6f925e7f0eaf3e7fb470
pritam roy <pritam20002001@gmail.com>
2024-05-23 23:40:37 +0530

    fixed the bcrypt error and also edited the index.js to only contain api endpoints of client

--- Routers/authentication/login.js
+++ Routers/authentication/login.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const dotenv = require("dotenv");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 dotenv.config()
